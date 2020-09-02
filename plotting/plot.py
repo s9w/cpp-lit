85f855ab8745b6a7d9188e904045d55213e103b7
Sebastian Werhausen <swerhausen@gmail.com>
2020-09-02 06:18:28 +0200

    improved the tooling a bit

--- plotting/plot.py
+++ plotting/plot.py
@@ -1,5 +1,6 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import os.path
 import numpy as np
 import mpld3
 
@@ -11,21 +12,29 @@ def get_pretty_name(name):
     return name
 
 def load_data(fn, color, release_baseline=0, debug_baseline=0):
+    if not os.path.isfile(fn):
+        return None
     comments = ["#"]
     # if baseline is supplied, ignore the baseline row itself so it doesn't get plotted
     if(debug_baseline != 0):
         comments.append("null")
     
-    names = np.vectorize(get_pretty_name)(np.loadtxt(fn, delimiter=' ', unpack=True, dtype=str, encoding="utf8", usecols=0, comments=comments))
-    release_mean, release_sd, debug_mean, debug_sd = np.loadtxt(fn, delimiter=' ', unpack=True, dtype=float, usecols=[1,2,3,4], comments=comments)
-    return {
+    names = np.loadtxt(fn, delimiter=' ', unpack=False, dtype=str, ndmin=1, encoding="utf8", usecols=0, comments=comments)
+    names = names.flatten().tolist() # yes, this is apparently the only way to force numby to behave like a sane person when there's only one element
+    if not names:
+        return None
+    names = np.asarray([get_pretty_name(name) for name in names])
+    release_mean, release_sd, debug_mean, debug_sd = np.loadtxt(fn, delimiter=' ', unpack=True, dtype=float, ndmin=2, usecols=[1,2,3,4], comments=comments)
+    ret_value = {
         "names": names,
-        "release_mean": release_mean-release_baseline,
+        "release_mean": release_mean - release_baseline,
         "release_sd": release_sd,
         "debug_mean": debug_mean-debug_baseline,
         "debug_sd": debug_sd,
         "color": color
     }
+    sort_data(ret_value)
+    return ret_value
 
 def sort_data(data):
     sort_indices = np.argsort(data["release_mean"]) # sort based on the mean build time of the release build
@@ -38,15 +47,17 @@ baseline_index = np.where(null_data["names"]=="null")
 baseline_release = null_data["release_mean"][baseline_index]
 baseline_debug = null_data["debug_mean"][baseline_index]
 
+datasets = []
 std_data = load_data("data_std.txt", "blue", baseline_release, baseline_debug)
 std_modules_data = load_data("data_std_modules.txt", "green", baseline_release, baseline_debug)
 misc_data = load_data("data_misc.txt", "red", baseline_release, baseline_debug)
 boost_data = load_data("data_boost.txt", "orange", baseline_release, baseline_debug)
-
-sort_data(std_data)
-sort_data(misc_data)
-sort_data(std_modules_data)
-sort_data(boost_data)
+if(std_data is not None):
+    datasets.append(std_data)
+if(std_modules_data is not None):
+    datasets.append(std_modules_data)
+if(misc_data is not None):
+    datasets.append(misc_data)
 
 def create_plot(datasets, output_fn, mode):
     number_of_entries = sum([len(dataset["names"]) for dataset in datasets])
@@ -94,7 +105,8 @@ def create_plot(datasets, output_fn, mode):
     # mpld3.save_html(fig, output_fn+".html")
     fig.savefig(output_fn + "_" + mode +".png")
 
-create_plot([std_data, std_modules_data, misc_data], "figure", "debug")
-create_plot([std_data, std_modules_data, misc_data], "figure", "release")
-create_plot([boost_data], "boost", "debug")
-create_plot([boost_data], "boost", "release")
+create_plot(datasets, "figure", "debug")
+create_plot(datasets, "figure", "release")
+if(boost_data is not None):
+    create_plot([boost_data], "boost", "debug")
+    create_plot([boost_data], "boost", "release")
--- run_numbers.ps1
+++ run_numbers.ps1
@@ -1,3 +1,14 @@
+$vcvars_dir = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvars64.bat"
+# $vcvars_dir = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Preview\VC\Auxiliary\Build\vcvars64.bat"
+
+$repetitions = 30
+$boost_dir = "D:\boost_1_74_0"
+$use_windows = $false
+$use_std_modules = $false
+$use_std_headers = $false
+
+"use_std_headers: {0}, use_std_modules: {1}, use_windows: {2}, boost: {3}" -f $use_std_headers, $use_std_modules, $use_windows, (Test-Path variable:boost_dir)
+
 function Invoke-CmdScript {
   param(
     [String] $scriptName
@@ -10,13 +21,22 @@ function Invoke-CmdScript {
     set-item Env:$varName $varValue
   }
 }
+if((-Not (Test-Path env:cpp_lit_invoked_vcvars)) -And (-Not $env:cpp_lit_invoked_vcvars)){
+   Write-Output "running invoke"
+   Invoke-CmdScript $vcvars_dir
+   $env:cpp_lit_invoked_vcvars = $true
+}
 
-$boost_dir = "C:\boost_1_74_0"
-# $vcvars_dir = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvars64.bat"
-$vcvars_dir = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Preview\VC\Auxiliary\Build\vcvars64.bat"
 
-Invoke-CmdScript $vcvars_dir
-
+$include_dirs = @()
+if(Test-Path variable:boost_dir){
+   $include_dirs += $boost_dir
+}
+$include_statement = ""
+Foreach($include_dir in $include_dirs){
+   $include_statement += "/I" + $include_dir + " "
+}
+Write-Output "include statement: " $include_statement
 
 function run_meas($category, $inc, $repeats, $config)
 {
@@ -32,11 +52,14 @@ function run_meas($category, $inc, $repeats, $config)
    }
 }
 
-$repetitions = 30
 $std_headers = "algorithm","any","array","atomic","bitset","cassert","cctype","cerrno","cfenv","cfloat","charconv","chrono","cinttypes","climits","clocale","cmath","compare","complex","concepts","condition_variable","csetjmp","csignal","cstdarg","cstddef","cstdint","cstdio","cstdlib","cstring","ctime","cuchar","cwchar","cwctype","deque","exception","execution","filesystem","forward_list","fstream","functional","future","initializer_list","iomanip","ios","iosfwd","iostream","istream","iterator","limits","list","locale","map","memory_resource","memory","mutex","new","numeric","optional","ostream","queue","random","ranges","ratio","regex","scoped_allocator","set","shared_mutex","sstream","stack","stdexcept","streambuf","string_view","string","system_error","thread","tuple","type_traits","typeindex","typeinfo","unordered_map","unordered_set","utility","valarray","variant","vector","version","bit","coroutine","numbers","span"
 $std_modules = "std_regex","std_filesystem","std_memory","std_threading","std_core"
 $boost_headers = "boost_variant2","boost_optional","boost_uuid","boost_asio","boost_atomic","boost_beast","boost_outcome","boost_any","boost_hana"
-$misc_headers = "null","windows","windows_mal"
+
+$misc_headers = @("null")
+if($use_windows){
+   $misc_headers += "windows","windows_mal"
+}
 
 if(Test-Path measurements){
    Remove-Item measurements -Recurse
@@ -48,15 +71,21 @@ Foreach($header in $misc_headers){
    run_meas "misc" $header $repetitions "Release"
    run_meas "misc" $header $repetitions "Debug"
 }
-Foreach($header in $boost_headers){
-   run_meas "boost" $header $repetitions "Release"
-   run_meas "boost" $header $repetitions "Debug"
+if(Test-Path variable:boost_dir){
+   Foreach($header in $boost_headers){
+      run_meas "boost" $header $repetitions "Release"
+      run_meas "boost" $header $repetitions "Debug"
+   }
 }
-Foreach($header in $std_modules){
-   run_meas "std_modules" $header $repetitions "Release"
-   run_meas "std_modules" $header $repetitions "Debug"
+if($use_std_modules){
+   Foreach($header in $std_modules){
+      run_meas "std_modules" $header $repetitions "Release"
+      run_meas "std_modules" $header $repetitions "Debug"
+   }
 }
-Foreach($header in $std_headers){
-   run_meas "std" $header $repetitions "Release"
-   run_meas "std" $header $repetitions "Debug"
+if($use_std_headers){
+   Foreach($header in $std_headers){
+      run_meas "std" $header $repetitions "Release"
+      run_meas "std" $header $repetitions "Debug"
+   }
 }
